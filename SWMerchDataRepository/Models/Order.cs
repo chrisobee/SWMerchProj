8fd0464c54b290d4a9397f5340e71f1c08c326cd
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 16:08:27 -0600

    Added Order Model

---
+++ SWMerchDataRepository/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SWMerchDataRepository.Models
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public string Email { get; set; }
+        public DateTime DatePurchased { get; set; }
+        public double Taxes { get; set; }
+    }
+}
