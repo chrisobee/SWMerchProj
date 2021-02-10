2692e7dbf211fbeca56a37c8ee0f215f28786982
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 16:04:28 -0600

    Added Customer Model

---
+++ SWMerchDataRepository/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SWMerchDataRepository.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ImgUrl { get; set; }
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+        [ForeignKey("UserSettings")]
+        public int UserSettingsId { get; set; }
+        public AdvancedUserSettings UserSettings { get; set; }
+    }
+}
