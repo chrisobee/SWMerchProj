40cfc10858fae2fff0ce0b2458e8ab4e8e82ea25
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 12:44:04 -0600

    Added Product Model

---
+++ SWMerchDataRepository/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SWMerchDataRepository.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ImgUrl { get; set; }
+        public string ShortDesc { get; set; }
+        public string LongDesc { get; set; }
+        public double MoralityPoints { get; set; }
+    }
+}
