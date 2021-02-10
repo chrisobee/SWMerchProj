704789d75c8eab5a58fdd46fd0714abde229dc34
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 16:25:04 -0600

    Added IRepositoryBase for Data Repository Pattern

--- SWMerchDataRepository/Data/ApplicationDbContext.cs
+++ SWMerchDataRepository/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SWMerchDataRepository.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,5 +14,10 @@ namespace SWMerchDataRepository.Data
         {
 
         }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<PurchasedProduct> PurchasedProducts { get; set; }
+        public DbSet<AdvancedUserSettings> UserSettings { get; set; }
     }
 }
