71b0c08432df874d31f1721b7428842b59e5d66c
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 12:41:22 -0600

    Added ApplicationDbContext service

---
+++ SWMerchDataRepository/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWMerchDataRepository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+    }
+}
