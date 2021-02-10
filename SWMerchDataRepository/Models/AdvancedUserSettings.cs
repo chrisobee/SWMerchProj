fb6c7b0488bd4d8f5f8f09c000a86c61c4fd3e7d
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-10 12:45:14 -0600

    Added AdvancedUserSettings Model

---
+++ SWMerchDataRepository/Models/AdvancedUserSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SWMerchDataRepository.Models
+{
+    public class AdvancedUserSettings
+    {
+        [Key]
+        public int UserSettingsId { get; set; }
+        public string Theme { get; set; }
+        public string Format { get; set; }
+    }
+}
