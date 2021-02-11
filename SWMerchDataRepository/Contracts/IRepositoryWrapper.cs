2078745a75432f97268b47a3bdf2929f4f4f17cf
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-11 10:05:52 -0600

    Added IRepositoryWrapper and RepositoryWrapper to access all repositories through one call

---
+++ SWMerchDataRepository/Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWMerchDataRepository.Contracts
+{
+    public interface IRepositoryWrapper
+    {
+        ICustomerRepository Customer { get; }
+        IOrderRepository Order { get; }
+        IProductRepository Product { get; }
+        IPurchasedProductRepository PurchasedProduct { get; }
+        IAdvancedUserSettingsRepository AdvancedUserSettings { get; }
+
+        void Save();
+    }
+}
---
+++ SWMerchDataRepository/RepositoryWrapper.cs
@@ -0,0 +1,88 @@
+﻿using SWMerchDataRepository.Contracts;
+using SWMerchDataRepository.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWMerchDataRepository
+{
+    public class RepositoryWrapper : IRepositoryWrapper
+    {
+        private ApplicationDbContext _context;
+        private ICustomerRepository _customer;
+        private IOrderRepository _order;
+        private IProductRepository _product;
+        private IPurchasedProductRepository _purchasedProduct;
+        private IAdvancedUserSettingsRepository _advancedUserSettings;
+
+        public ICustomerRepository Customer
+        {
+            get
+            {
+                if(_customer == null)
+                {
+                    _customer = new CustomerRepository(_context);
+                }
+                return _customer;
+            }
+        }
+
+        public IOrderRepository Order
+        {
+            get
+            {
+                if(_order == null)
+                {
+                    _order = new OrderRepository(_context);
+                }
+                return _order;
+            }
+        }
+
+        public IProductRepository Product
+        {
+            get
+            {
+                if(_product == null)
+                {
+                    _product = new ProductRepository(_context);
+                }
+                return _product;
+            }
+        }
+
+        public IPurchasedProductRepository PurchasedProduct
+        {
+            get
+            {
+                if(_purchasedProduct == null)
+                {
+                    _purchasedProduct = new PurchasedProductRepository(_context);
+                }
+                return _purchasedProduct;
+            }
+        }
+
+        public IAdvancedUserSettingsRepository AdvancedUserSettings
+        {
+            get
+            {
+                if(_advancedUserSettings == null)
+                {
+                    _advancedUserSettings = new AdvancedUserSettingsRepository(_context);
+                }
+                return _advancedUserSettings;
+            }
+        }
+
+        public RepositoryWrapper(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
