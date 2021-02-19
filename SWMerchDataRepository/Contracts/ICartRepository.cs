2d8479f1cbe0f5d634469ae782db18e49b7e4660
Chris O'Brien <chrisobrien2000@outlook.com>
2021-02-19 14:10:04 -0600

    Added methods to ICartRepository

--- SWMerchDataRepository/Contracts/ICartRepository.cs
+++ SWMerchDataRepository/Contracts/ICartRepository.cs
@@ -2,10 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SWMerchDataRepository.Contracts
 {
     public interface ICartRepository: IRepositoryBase<Cart>
     {
+        public Task AddProductToCart(int cartId, int productId);
+        public Task SetQuantity(int cartId, int productId, int qty);
+        public Task RemoveProductFromCart(int cartId, int productId);
     }
 }
